7851a62602adf233da9ae7861eb6f847d150ba28
Arthur Bourbousson <arthur.bourbousson@kitware.com>
2022-12-15 15:21:42 +0100

    [refact] Make Mahalanobis Distance function independent
    Make Mahalanobis Distance Cost function independent of linear
    interpolation class.
    Linear interpolation class will be replaced and need to be changed in function using it.

--- slam_lib/include/LidarSlam/ExternalSensorManagers.h
+++ slam_lib/include/LidarSlam/ExternalSensorManagers.h
@@ -20,6 +20,7 @@
 
 #include "LidarSlam/CeresCostFunctions.h" // for residual structure + ceres
 #include "LidarSlam/Utilities.h"
+#include "LidarSlam/MotionModel.h"
 #include <list>
 #include <cfloat>
 #include <mutex>
