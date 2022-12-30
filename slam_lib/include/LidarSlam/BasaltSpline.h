7c265de38ce77b658521bb478bd0094aa495d4b1
Arthur Bourbousson <arthur.bourbousson@kitware.com>
2022-12-30 17:20:19 +0100

    [feat] Start algorithm for quaternion interpolation

--- slam_lib/include/LidarSlam/BasaltSpline.h
+++ slam_lib/include/LidarSlam/BasaltSpline.h
@@ -11,7 +11,45 @@ namespace LidarSlam
 {
 namespace Interpolation
 {
+// ---------------------------------------------------------------------------
+//   Rotation Models
+// ---------------------------------------------------------------------------
 
+// ---------------------------- Rotation Spline --------------------------------
+
+// Non-Uniform Catmull–Rom-Like Rotation Splines
+// Using a spherical Bézier curves of "degree" cubic
+// Using https://splines.readthedocs.io/en/latest/rotation/catmull-rom-non-uniform.html in C++
+
+class RotSpline : public IModel
+{
+  using SO3d = Sophus::SO3<double>;
+
+  public:
+    RotSpline() = delete;
+    RotSpline(const std::vector<PoseStamped>& vecPose)
+    {
+      // for (auto it = vecPose.begin(); it != vecPose.end(); ++it)
+      // {
+      //     VecKnots.push_back(SO3d{Eigen::Quaterniond{it->Pose.linear()}});
+      //   // if (it != vecPose.begin())
+      //   //TODO canonize the quaternions (see slines site)
+      // }
+    }
+    Eigen::Isometry3d operator()(double t) const
+    {
+      return (Eigen::Isometry3d::Identity());
+    }
+    void RecomputeModel(const std::vector<PoseStamped> &vecPose)
+    {
+      (void)vecPose;
+    }
+
+  private:
+    std::vector<SO3d> VecKnots;
+    std::vector<std::pair<SO3d, SO3d>> VecCtrlPoints;
+    std::vector<double> VecTime;
+};
 
 }  // end of Interpolation namespace
 }  // end of LidarSlam namespace
