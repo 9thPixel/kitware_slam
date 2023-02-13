277176e0bb4ad0c3b2dd683d01833c78ef01fe06
Tong Fu <tong.fu@kitware.com>
2023-02-13 16:55:53 +0100

    [refact][style] Redefine getter/setter for PGOConstraint

--- slam_lib/src/Slam.cxx
+++ slam_lib/src/Slam.cxx
@@ -609,7 +609,7 @@ bool Slam::OptimizeGraph()
 {
   #ifdef USE_G2O
   // Check if graph can be optimized
-  if (!this->LmHasData() && !this->GpsHasData() && !UsePGOConstraints[LOOP_CLOSURE])
+  if (!this->LmHasData() && !this->GpsHasData() && !this->UsePGOConstraints[LOOP_CLOSURE])
   {
     PRINT_WARNING("No external constraint found, graph cannot be optimized");
     return false;
@@ -635,7 +635,7 @@ bool Slam::OptimizeGraph()
   bool externalConstraint = false;
 
   // Look for loop closure constraints
-  if (UsePGOConstraints[LOOP_CLOSURE])
+  if (this->UsePGOConstraints[LOOP_CLOSURE])
   {
     // Detect loop closure
     auto itRevisitedState = this->LogStates.begin();
@@ -660,7 +660,7 @@ bool Slam::OptimizeGraph()
   }
 
   // Look for landmark constraints
-  if (UsePGOConstraints[LANDMARK] && this->LmHasData())
+  if (this->UsePGOConstraints[LANDMARK] && this->LmHasData())
   {
     // Allow the rotation of the covariances when interpolating the measurements
     this->SetLandmarkCovarianceRotation(true);
@@ -696,7 +696,7 @@ bool Slam::OptimizeGraph()
   }
 
   // Look for GPS constraints
-  if (UsePGOConstraints[PGO_GPS] && this->GpsHasData())
+  if (this->UsePGOConstraints[PGO_GPS] && this->GpsHasData())
   {
     graphManager.AddExternalSensor(this->GpsManager->GetCalibration(), int(ExternalSensor::GPS));
     for (auto& s : this->LogStates)
@@ -759,6 +759,20 @@ bool Slam::OptimizeGraph()
   PRINT_ERROR("SLAM graph optimization requires G2O, but it was not found.");
   return false;
   #endif  // USE_G2O
+}
+
+//-----------------------------------------------------------------------------
+void Slam::EnablePGOConstraint(PGOConstraint constraint, bool enabled)
+{
+  this->UsePGOConstraints[constraint] = enabled;
+}
+
+//-----------------------------------------------------------------------------
+bool Slam::IsPGOConstraintEnabled(PGOConstraint constraint) const
+{
+  if (!this->UsePGOConstraints.count(constraint))
+    return false;
+  return this->UsePGOConstraints.at(constraint);
 }
 
 //-----------------------------------------------------------------------------
